5bb231000d8ed7c5428b763c5bb718bd351002ae
LaurentP03 <Laurentpimentelacc@gmail.com>
2025-07-09 08:26:47 -0300

    chore/cria estrutura de pastas e arquivos base

---
+++ sources/script.js
@@ -0,0 +1,9 @@
+const sltFiltros = document.getElementById("sltFiltros");
+
+function gerarRelatorio() {
+}
+
+sltFiltros.addEventListener("change", filtrarRelatorio);
+
+function filtrarRelatorio() {
+}
