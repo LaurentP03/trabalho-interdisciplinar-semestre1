ee4a55e6548e6204b8412125f697c5329af1390d
Taylor051 <devtaylorcaetano@gmail.com>
2025-07-23 17:27:46 -0300

    criei filtrarNome e escolherFiltro

--- sources/script.js
+++ sources/script.js
@@ -2,7 +2,13 @@ document.body.onload = gerarRelatorio;
 const sltFiltros = document.getElementById("sltFiltros");
 
 function gerarRelatorio() {
+    const antiga = document.querySelector("#tabelaCandidatos");
+    if (antiga) {
+        antiga.remove();
+    }
     const tabela = document.createElement("table");
+    tabela.classList.add("tabela-candidatos");
+    tabela.id = "tabelaCandidatos";
     const cabecalho = document.createElement("tr");
 
     for (let i = 0; i < titulos.length; i++) {
@@ -21,7 +27,7 @@ function gerarRelatorio() {
         const tdNascimento = document.createElement("td");
         tdNascimento.innerHTML = vetNascimento[i];
         const tdPontuacao = document.createElement("td");
-        tdPontuacao.innerHTML = vetNascimento[i];
+        tdPontuacao.innerHTML = vetPontuacao[i];
         linha.appendChild(tdNome);
         linha.appendChild(tdCpf);
         linha.appendChild(tdNascimento);
@@ -31,7 +37,50 @@ function gerarRelatorio() {
     document.getElementById("container").appendChild(tabela);
 }
 
-sltFiltros.addEventListener("change", filtrarRelatorio);
+sltFiltros.addEventListener("change", escolherFiltro);
 
-function filtrarRelatorio() {
+function escolherFiltro() {
+    const filtro = sltFiltros.value;
+    
+
+    switch (filtro) {
+        case "nome":
+            filtrarNome();
+            break;
+        case "dataNascimento":
+            filtrarDataDeNascimento();
+
+            break;
+        case "pontuacao":
+            filtrarPontuacao();
+
+            break;
+
+        default:
+            gerarRelatorio();
+            break;
+    }
+}
+
+function filtrarNome() {
+    const n = vetCandidatos.length;
+
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = i; j < n; j++) {
+
+            if (vetCandidatos[i].localeCompare(vetCandidatos[j]) > 0) {
+                [vetCandidatos[i], vetCandidatos[j]] = [vetCandidatos[j], vetCandidatos[i]];
+                [vetCPF[i], vetCPF[j]] = [vetCPF[j], vetCPF[i]];
+                [vetNascimento[i], vetNascimento[j]] = [vetNascimento[j], vetNascimento[i]];
+                [vetPontuacao[i], vetPontuacao[j]] = [vetPontuacao[j], vetPontuacao[i]];
+
+                
+
+
+            }
+
+        }
+
+    }
+    gerarRelatorio();
 }
