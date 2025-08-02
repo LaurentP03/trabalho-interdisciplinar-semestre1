66cfdc17a85adfd31060fefc6a7548c324e3c0a7
Lau-rent03 <Laurentpimentelacc@gmail.com>
2025-08-02 13:38:42 -0300

    Comentando vetores

--- sources/vetores.js
+++ sources/vetores.js
@@ -10,8 +10,10 @@ var vetPontuacao = [831, 814, 725, 1248, 1234, 1227, 1244, 723, 750, 1178, 1047,
 
 var titulos = ["Nome do Candidato", "CPF", "Nascimento", "Pontuação", "Indicação"];
 
+//vetor que guarda a posição dos candidatos filtrados
 var candidatosFiltrados = [];
 
+//vetores organizados para a tabela top 10
 var vetPontuacaoDecrescente = [];
 var vetCandidatosDescrescente = [];
 var vetNascimentoDescrescente = [];
